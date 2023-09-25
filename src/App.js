bec8a99e2332652a6ba06cdf88fa0e56191b6f5f
Pablo <ezekielgonzalezr@gmail.com>
2023-09-25 11:31:35 -0600

    Set up linters

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import Home from './pages/Home';
 import {
   Route, RouterProvider, createBrowserRouter, createRoutesFromElements,
 } from 'react-router-dom';
+import Home from './pages/Home';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Home />} />
-  ));
+    <Route path="/" element={<Home />} />,
+  ),
+);
 
 function App() {
   return (
