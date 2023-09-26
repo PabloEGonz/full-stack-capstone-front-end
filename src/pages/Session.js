0be34b723a1522b5dcd4f6016dee0c51a5f34c99
Pablo <ezekielgonzalezr@gmail.com>
2023-09-25 21:32:28 -0600

    Use login component

--- src/pages/Session.js
+++ src/pages/Session.js
@@ -1,25 +1,9 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchUser } from '../redux/users/usersSlice';
+import Login from '../components/Login';
 
-const Sessions = () => {
-  const dispatch = useDispatch();
-  return (
-    <div>
-      <form action="log-in" method="post">
-        <label htmlFor="user-name">
-          User name
-          <input type="text" name="user-name" id="user-name" />
-        </label>
-        <button
-          type="button"
-          onClick={() => dispatch(fetchUser('ezek'))}
-        >
-          Log in
-        </button>
-      </form>
-    </div>
-  );
-};
+const Sessions = () => (
+  <div>
+    <Login />
+  </div>
+);
 
 export default Sessions;
