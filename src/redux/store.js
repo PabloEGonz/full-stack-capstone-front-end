4931553e0f9bf708728789f913607963c86af4b5
Ezra <ezraermy@gmail.com>
2023-09-27 18:16:28 +0300

    Update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './users/usersSlice';
+import reservationsSlice from './reservations/reservationsSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    reservations: reservationsSlice,
   },
 });
 export default store;
