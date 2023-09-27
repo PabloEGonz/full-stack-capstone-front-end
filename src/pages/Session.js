0696e24a6e72fc38480b1325142ae855ba6eb2c5
Pablo <ezekielgonzalezr@gmail.com>
2023-09-26 20:21:16 -0600

    Create session page

--- src/pages/Session.js
+++ src/pages/Session.js
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Login from '../components/Login';
 import Signin from '../components/Signin';
 
-const Session = () => (
-  <div>
-    <Login />
-    <Signin />
-  </div>
-);
+const Session = () => {
+  const [logIn, SetLogIn] = useState('show active');
+  const [signIn, SetSignIn] = useState('');
+
+  const toggleVissibility = (target, off) => {
+    off(' ');
+    target('show active');
+  };
+  return (
+    <div className="container session-form">
+      <ul className="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
+        <li className="nav-item" role="presentation">
+          <a
+            className={`nav-link ${logIn}`}
+            id="tab-login"
+            data-mdb-toggle="pill"
+            href="#pills-login"
+            role="tab"
+            aria-controls="pills-login"
+            aria-selected="false"
+            onClick={() => toggleVissibility(SetLogIn, SetSignIn)}
+          >
+            Login
+          </a>
+        </li>
+        <li className="nav-item" role="presentation">
+          <a
+            className={`nav-link ${signIn}`}
+            id="tab-register"
+            data-mdb-toggle="pill"
+            href="#pills-register"
+            role="tab"
+            aria-controls="pills-register"
+            aria-selected="true"
+            onClick={() => toggleVissibility(SetSignIn, SetLogIn)}
+          >
+            Register
+          </a>
+        </li>
+      </ul>
+      <div className="tab-content">
+        <div className={`tab-pane fade ${logIn}`} id="pills-login" role="tabpanel" aria-labelledby="tab-login">
+          <Login />
+        </div>
+        <div className={`tab-pane fade ${signIn}`} id="pills-register" role="tabpanel" aria-labelledby="tab-register">
+          <Signin />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Session;
