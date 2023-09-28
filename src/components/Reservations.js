08e3138e4d6f402bc74105fe5f3fe81aa53c7ed4
Ezra <ezraermy@gmail.com>
2023-09-28 20:26:29 +0300

    Display the reservations in a swipeable carousel format

--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -8,7 +8,71 @@ const Reservations = () => {
   const user = useSelector((state) => state.user.id);
   const dispatch = useDispatch();
 
- 
+  useEffect(() => {
+    dispatch(getReservations(user));
+  }, [dispatch]);
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <div className="spinner" />
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div className="error-container">
+        <h2>Oopps somethings went wrong.PLease try again!</h2>
+        <p>{error}</p>
+      </div>
+    );
+  }
+  if (!user) {
+    return (
+      <div className="error-container">
+        <h2>Please login to see your reservations</h2>
+      </div>
+    );
+  } return (
+    <div className="reservation-page">
+      <h1 className="reserve-title">My Reservations</h1>
+      <Swiper>
+        {reservations.map((reservation) => (
+          <SwiperSlide className="reservation-info" key={reservation.id}>
+            <div>
+              <p>
+                <strong>Reservation Date:&ensp;</strong>
+                {reservation.reservation_date}
+              </p>
+            </div>
+            <div>
+              <p>
+                <strong>Due Date:&ensp;</strong>
+                {reservation.due_date}
+              </p>
+              <p>
+                <strong>Fee:&ensp;</strong>
+                {reservation.service_fee}
+              </p>
+              <p>
+                <strong>Car:&ensp;</strong>
+                {reservation.car.name}
+              </p>
+              <p>
+                <strong>Location:&ensp;</strong>
+                {reservation.car.location}
+              </p>
+              <p>
+                <strong>About:&ensp;</strong>
+                {reservation.car.description}
+              </p>
+              <img src={reservation.car.image} alt={`${reservation.car.name}`} />
+            </div>
+          </SwiperSlide>
+
+        ))}
+      </Swiper>
+    </div>
+  );
 };
 
 export default Reservations;
