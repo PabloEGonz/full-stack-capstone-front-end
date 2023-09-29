85a91041af0249ae030e07bfd50390760bd8f5ee
yosaddis <email.to.yoseph@gmail.com>
2023-09-30 01:09:11 +0300

    update Reservation.js

--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -1,77 +1,110 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { getReservations } from '../redux/reservations/reservationsSlice';
-
-const Reservations = () => {
-  const { reservations, isLoading, error } = useSelector((store) => store.reservations);
-  const user = useSelector((state) => state.user.id);
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { createReserve, getReservations } from '../redux/reservations/reservationsSlice';
+import { getCars } from '../redux/cars/carsSlice';
+import '../css/ReservationForm.css';
+const Reservation = () => {
+  const cars = useSelector((state) => state.cars.cars);
+  const userId = useSelector((state) => state.user.id);
   const dispatch = useDispatch();
-
+  const navigate = useNavigate();
   useEffect(() => {
-    dispatch(getReservations(user));
-  }, [dispatch, user]);
-  if (isLoading) {
-    return (
-      <div className="loading">
-        <div className="spinner" />
-      </div>
-    );
-  }
-  if (error) {
-    return (
-      <div className="error-container">
-        <h2>Oopps somethings went wrong.PLease try again!</h2>
-        <p>{error}</p>
-      </div>
-    );
-  }
-  if (!user) {
-    return (
-      <div className="error-container">
-        <h2>Please login to see your reservations</h2>
-      </div>
-    );
-  } return (
-    <div className="reservation-page">
-      <h1 className="reserve-title">My Reservations</h1>
-      <Swiper>
-        {reservations.map((reservation) => (
-          <SwiperSlide className="reservation-info" key={reservation.id}>
-            <div>
-              <p>
-                <strong>Reservation Date:&ensp;</strong>
-                {reservation.reservation_date}
-              </p>
-            </div>
-            <div>
-              <p>
-                <strong>Due Date:&ensp;</strong>
-                {reservation.due_date}
-              </p>
-              <p>
-                <strong>Fee:&ensp;</strong>
-                {reservation.service_fee}
-              </p>
-              <p>
-                <strong>Car:&ensp;</strong>
-                {reservation.car.name}
-              </p>
-              <p>
-                <strong>Location:&ensp;</strong>
-                {reservation.car.location}
-              </p>
-              <p>
-                <strong>About:&ensp;</strong>
-                {reservation.car.description}
-              </p>
-              <img src={reservation.car.image} alt={`${reservation.car.name}`} />
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    dispatch(getCars());
+    dispatch(getReservations(userId));
+  }, [dispatch, userId]);
+  const [reserve, setReserve] = useState({
+    user_id: userId,
+    reservation_date: '',
+    due_date: '',
+    service_fee: '',
+    car_id: '',
+  });
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setReserve((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  const submit = async () => {
+    if (!reserve.car_id || !reserve.reservation_date || !reserve.due_date || !reserve.service_fee) {
+      alert('Please fill in all required fields.');
+      return;
+    }
+    const reservationDate = new Date(reserve.reservation_date);
+    const dueDate = new Date(reserve.due_date);
+    if (reservationDate >= dueDate) {
+      alert('Reservation date must be before due date.');
+      return;
+    }
+    await dispatch(createReserve(reserve));
+    navigate('/reservations');
+  };
+  return (
+    <div className="container mt-5">
+      <h2>Create Reservation</h2>
+      <form className="reservation-form">
+        <div className="mb-3">
+          <label htmlFor="reservationDate" className="form-label">Reservation Date</label>
+          <input
+            type="date"
+            id="reservationDate"
+            name="reservation_date"
+            value={reserve.reservation_date}
+            onChange={handleInputChange}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="dueDate" className="form-label">Due Date</label>
+          <input
+            type="date"
+            id="dueDate"
+            name="due_date"
+            value={reserve.due_date}
+            onChange={handleInputChange}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="serviceFee" className="form-label">Service Fee</label>
+          <div className="input-group">
+            <span className="input-group-text">$</span>
+            <input
+              type="number"
+              id="serviceFee"
+              name="service_fee"
+              value={reserve.service_fee}
+              onChange={handleInputChange}
+              className="form-control"
+              required
+            />
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="car" className="form-label">Select a Car</label>
+          <select
+            id="car"
+            name="car_id"
+            value={reserve.car_id}
+            onChange={handleInputChange}
+            className="form-select"
+            required
+          >
+            <option value="">Select a car</option>
+            {cars.map((car) => (
+              <option key={car.id} value={car.id}>
+                {car.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button type="button" onClick={submit} className="btn btn-primary">Create Reservation</button>
+      </form>
     </div>
   );
 };
-
-export default Reservations;
+export default Reservation;
