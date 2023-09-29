407dd106f9ccddc008aac66c30d2891a535e5e14
Ezra <ezraermy@gmail.com>
2023-09-29 15:36:28 +0300

    Add box shadow and margin

--- src/pages/Reservations.js
+++ src/pages/Reservations.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getReservations } from '../redux/reservations/reservationsSlice';
+import '../css/Reservations.css';
 
 const Reservations = () => {
   const { reservations, isLoading, error } = useSelector((store) => store.reservations);
