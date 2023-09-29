f8749edee669fc4fd0486da4a376eca2cfe27f2e
Ezra <ezraermy@gmail.com>
2023-09-29 22:33:27 +0300

    Create a logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../css/Navbar.css';
+import logo from '../assets/logo.png';
 
 const Navbar = () => (
   <nav className="navbar navbar-body bg-body-tertiary fixed-top">
@@ -8,7 +9,9 @@ const Navbar = () => (
       <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon" />
       </button>
-      <a className="navbar-brand" href="/">Rent a Car</a>
+      <a className="navbar-brand" href="/">
+        <img src={logo} alt="Rent a Car Logo" />
+      </a>
       <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
         <div className="offcanvas-header">
           <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Rent a Car</h5>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,14 +6,27 @@ const Home = () => {
   const userPresent = useSelector((state) => state.user.id);
   if (!userPresent) {
     return (
-      <div>
-        <h1>You need to log-in or sign-in to continue</h1>
-        <Link className="btn btn-primary" to="/session">Login</Link>
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <div className="alert alert-danger" role="alert">
+              You need to log-in or sign-in to continue
+            </div>
+            <Link className="btn btn-primary" to="/session">Login</Link>
+          </div>
+        </div>
       </div>
     );
   }
   return (
-    <h1>Home</h1>
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <h1 className="text-center">Welcome</h1>
+        </div>
+      </div>
+    </div>
   );
 };
+
 export default Home;
