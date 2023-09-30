71439b4e9a9fdb4a098d49e35a086a40c45c58e4
Ezra <ezraermy@gmail.com>
2023-09-30 03:12:36 +0300

    Create login first before new reservation

--- src/components/Reserve.js
+++ src/components/Reserve.js
@@ -66,7 +66,20 @@ const Reservation = () => {
     );
   }
 
-  
+  if (!userId) {
+    return (
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <div className="alert alert-danger" role="alert">
+              You need to log-in or sign-in to continue
+            </div>
+            <Link className="btn btn-primary" to="/session">Login</Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="container mt-5">
