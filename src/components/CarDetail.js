563440f7eecf9d255bbe76bacd34443fcde5d0b6
yosaddis <email.to.yoseph@gmail.com>
2023-10-01 06:43:48 +0300

    Update the car detial page and style

--- src/components/CarDetail.js
+++ src/components/CarDetail.js
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import '../assets/CarDetails.css';
 
 function CarDetail() {
   const { carId } = useParams();
@@ -8,11 +9,45 @@ function CarDetail() {
   // Find the specific car with the matching ID
   const car = cars.find((c) => c.id === Number(carId));
 
+  // return (
+  //   <div>
+  //     {/* Display car details here */}
+  //     <h1>{car.name}</h1>
+  //     {/* Display other car details */}
+  //   </div>
+  // );
+
   return (
-    <div>
-      {/* Display car details here */}
-      <h1>{car.name}</h1>
-      {/* Display other car details */}
+    <div className="car-details">
+      <h1 className="car-name-details-mobile">{car.name}</h1>
+      <div className="car-image-table-container">
+        <div className="car-image-container">
+          <img src={car.image} alt={car.name} className="car-image-details" />
+        </div>
+        <div className="car-details-container">
+          <h1 className="car-name-details">{car.name}</h1>
+          <table className="car-details-table">
+            <tbody>
+              <tr>
+                <th>Type</th>
+                <td>{car.car_type}</td>
+              </tr>
+              <tr>
+                <th>Daily Rate</th>
+                <td>{car.daily_rate}</td>
+              </tr>
+              <tr>
+                <th>Location</th>
+                <td>{car.location}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <p className="car-description">{car.description}</p>
+      <div className="button-container">
+        <button type="button" className="reserve-button">Reserve</button>
+      </div>
     </div>
   );
 }
