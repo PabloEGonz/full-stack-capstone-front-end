ae30d5ea4e734342fdce6f65c9896528faa296ef
Pablo <ezekielgonzalezr@gmail.com>
2023-10-01 11:57:32 -0600

    Remove unnecessary call to API

--- src/components/Reserve.js
+++ src/components/Reserve.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate, Link } from 'react-router-dom';
-import { createReserve, getReservations } from '../redux/reservations/reservationsSlice';
+import { createReserve } from '../redux/reservations/reservationsSlice';
 import { getCars } from '../redux/cars/carsSlice';
 import '../css/ReservationForm.css';
 
@@ -15,7 +15,6 @@ const Reservation = () => {
 
   useEffect(() => {
     dispatch(getCars());
-    dispatch(getReservations(userId));
   }, [dispatch, userId]);
 
   const [reserve, setReserve] = useState({
