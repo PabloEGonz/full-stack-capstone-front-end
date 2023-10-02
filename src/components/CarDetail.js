066f0f4bf41cc1a4f47330a80f55f62dfaab6223
Ezra <ezraermy@gmail.com>
2023-10-02 21:32:27 +0300

    Update the unit test

--- src/components/CarDetail.js
+++ src/components/CarDetail.js
@@ -45,11 +45,7 @@ function CarDetail() {
         </div>
       </div>
       <p className="car-description">{car.description}</p>
-      <div className="button-container">
-        <Link to="/reservations/new" className="nav-link">
-          <button type="button" className="reserve-button">Reserve</button>
-        </Link>
-      </div>
+     
     </div>
   );
 }
