e289448ec0eeaed3fa22f5b6ba215c08d6f48f1d
Ezra <ezraermy@gmail.com>
2023-10-02 21:33:09 +0300

    Pass query parameter to the reserve button

--- src/components/CarDetail.js
+++ src/components/CarDetail.js
@@ -45,7 +45,11 @@ function CarDetail() {
         </div>
       </div>
       <p className="car-description">{car.description}</p>
-     
+      <div className="button-container">
+        <Link to={`/reservations/new?carId=${car.id}&carName=${encodeURIComponent(car.name)}`} className="nav-link">
+          <button type="button" className="reserve-button">Reserve</button>
+        </Link>
+      </div>
     </div>
   );
 }
